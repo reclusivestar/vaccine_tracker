11d3caa2511db40e9a3197e8587d6a282e471ed6
reclusivestar <tanmaysk1@gmail.com>
2021-01-16 20:55:00 -0800

    time slider works but not with custom range

--- src/Map/Map.jsx
+++ src/Map/Map.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, setState } from "react";
+import { useState, useEffect } from "react";
 import {
     ComposableMap,
     Geographies,
@@ -6,10 +6,11 @@ import {
     Marker,
     Annotation
   } from "react-simple-maps";
-import { scaleQuantize } from "d3-scale";
+import { scaleQuantize, scaleTime } from "d3-scale";
 import { extent } from "d3-array";
 import { geoCentroid } from "d3-geo";
 import axios from 'axios';
+import Time from './Time';
 
 const geoUrl = "https://cdn.jsdelivr.net/npm/us-atlas@3/states-10m.json";
 
@@ -23,15 +24,25 @@ const offsets = {
     DE: [33, 0],
     MD: [47, 10],
     DC: [49, 21]
-  };
+};
 
 const Map = (props) => {
   const url = "https://datausa.io/api/data?drilldowns=State&measures=Population";
   const [allStates, setAllStates] = useState([]);
   const [timeSeries, setTimeSeries] = useState([]);
   const [populations, setPopulations] = useState([]);
+  const [selectedTime, setSelectedTime] = useState(new Date());
+  const [startDate, setStartDate] = useState([]);
+  const [endDate, setEndDate] = useState([]);
+
   const colors = ["#ffedea", "#ffcec5", "#ffad9f", "#ff8a75", 
   "#ff5533", "#e2492d", "#be3d26", "#9a311f", "#782618"];
+
+  const dateTicks = scaleTime()
+      .domain([startDate, endDate])
+      .ticks(16)
+      //.ticks(parseInt((endDate - startDate) / (1000 * 60 * 60 * 24), 10))
+      .map((d) => +d);
 
   function extractNumbers() {
     let data = [];
@@ -65,28 +76,32 @@ const Map = (props) => {
   useEffect(() => {
     let data = filterField(props.data, "people_total");
     setTimeSeries(data);
-    setAllStates(filterDate(data, "12/18/2020"));
+    setDateRange(data);
+    setAllStates(filterDate(data, selectedTime));
     getAllPopulations();
   }, [props.data]);
 
+  useEffect(() => {
+    setAllStates(filterDate(timeSeries, selectedTime));
+  }, [selectedTime]);
+
+  function setDateRange(states) {
+    let dates = [];
+    states.forEach(state => {
+      state.data.forEach(data => dates.push(new Date(data.date)));
+    });
+    let maxDate = new Date(Math.max.apply(null,dates));
+    let minDate = new Date(Math.min.apply(null,dates));
+    setStartDate(minDate);
+    setEndDate(maxDate);
+    setSelectedTime(minDate);
+  }
 
   function getRate(val, count){
     let pop = populations.filter(state => state["ID State"].slice(-2) === val)[0];
     if (pop) //some states not in population data
       return count / pop.Population * 100;
     return 0;
-  }
-
-  function filterLatest(){
-    let latest = [];
-    props.data.forEach(field => {
-        let newData = [];
-        field.data.forEach(row => {
-            newData.push({...row, data: row.data.slice(row.data.length - 1)});
-        });
-        latest.push({...field, data: newData});
-    });
-    return latest;
   }
 
   function filterField(allData, title){
@@ -110,6 +125,7 @@ const Map = (props) => {
       let newData = state.data.filter(data => data.date === date);
       filtered.push({...state, data: newData});
     });
+    console.log(filtered, date);
     return filtered;
   }
 
@@ -121,6 +137,7 @@ const Map = (props) => {
 
   console.log(allStates);
   console.log(extractNumbers());
+  console.log(timeSeries);
 
   function legend(){
     let domains = extractDomain();
@@ -142,61 +159,77 @@ const Map = (props) => {
   }
 
   return (
-    <div style={{display: "flex",  justifyContent: "center"}}>
-      <div style={{width: "80%"}}>
-      <ComposableMap projection="geoAlbersUsa">
-        <Geographies geography={geoUrl}>
-        {({ geographies }) => (
-          <>
-            {geographies.map(geo => {
-              //const cur = data.find(s => s.id === geo.id);
-              const cur = allStates.find(s => s.val === geo.id);
-              return (
-                <Geography
-                  key={geo.rsmKey}
-                  geography={geo}
-                  fill={cur && cur.data[0] ? colorScale(getRate(cur.val, cur.data[0].count)) : "#A9A9A9"}
-                  onClick={handleClick(geo.id)}
-                  /*onMouseEnter={() => console.log(geo.id)}*/
-                />
-              );
-            })}
-            {geographies.map(geo => {
-              const centroid = geoCentroid(geo);
-              const cur = allStates.find(s => s.val === geo.id);
-              return (
-                <g key={geo.rsmKey + "-name"}>
-                  {cur &&
-                    centroid[0] > -160 &&
-                    centroid[0] < -67 &&
-                    (Object.keys(offsets).indexOf(cur.id) === -1 ? (
-                      <Marker coordinates={centroid}>
-                        <text y="2" fontSize={14} textAnchor="middle">
-                          {cur.id}
-                        </text>
-                      </Marker>
-                    ) : (
-                      <Annotation
-                        subject={centroid}
-                        dx={offsets[cur.id][0]}
-                        dy={offsets[cur.id][1]}
-                      >
-                        <text x={4} fontSize={14} alignmentBaseline="middle">
-                          {cur.id}
-                        </text>
-                      </Annotation>
-                    ))}
-                </g>
-              );
-            })}
-          </>
-        )}
-      </Geographies>
-      </ComposableMap>
+    <div>
+      <div style={{display: "flex",  justifyContent: "center"}}>
+        <div style={{width: "80%"}}>
+        <ComposableMap projection="geoAlbersUsa">
+          <Geographies geography={geoUrl}>
+          {({ geographies }) => (
+            <>
+              {geographies.map(geo => {
+                //const cur = data.find(s => s.id === geo.id);
+                const cur = allStates.find(s => s.val === geo.id);
+                return (
+                  <Geography
+                    key={geo.rsmKey}
+                    geography={geo}
+                    fill={cur && cur.data[0] ? colorScale(getRate(cur.val, cur.data[0].count)) : "#A9A9A9"}
+                    onClick={handleClick(geo.id)}
+                    /*onMouseEnter={() => console.log(geo.id)}*/
+                  />
+                );
+              })}
+              {geographies.map(geo => {
+                const centroid = geoCentroid(geo);
+                const cur = allStates.find(s => s.val === geo.id);
+                return (
+                  <g key={geo.rsmKey + "-name"}>
+                    {cur &&
+                      centroid[0] > -160 &&
+                      centroid[0] < -67 &&
+                      (Object.keys(offsets).indexOf(cur.id) === -1 ? (
+                        <Marker coordinates={centroid}>
+                          <text y="2" fontSize={14} textAnchor="middle">
+                            {cur.id}
+                          </text>
+                        </Marker>
+                      ) : (
+                        <Annotation
+                          subject={centroid}
+                          dx={offsets[cur.id][0]}
+                          dy={offsets[cur.id][1]}
+                        >
+                          <text x={4} fontSize={14} alignmentBaseline="middle">
+                            {cur.id}
+                          </text>
+                        </Annotation>
+                      ))}
+                  </g>
+                );
+              })}
+            </>
+          )}
+        </Geographies>
+        </ComposableMap>
+        </div>
+        {legend()}
       </div>
-      {legend()}
+    <Time start={startDate} end={endDate} selectDate={setSelectedTime}/>
     </div>
   );
 };
 
 export default Map;
+
+
+  /*function filterLatest(){
+    let latest = [];
+    props.data.forEach(field => {
+        let newData = [];
+        field.data.forEach(row => {
+            newData.push({...row, data: row.data.slice(row.data.length - 1)});
+        });
+        latest.push({...field, data: newData});
+    });
+    return latest;
+  }*/
